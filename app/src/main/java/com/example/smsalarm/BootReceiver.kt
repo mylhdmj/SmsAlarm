6bd55ffbf0080e8bca18bcfd7587fb04a5fdfb45
mylhdmj <zhou75i@163.com>
2026-05-29 11:29:46 +0800

    Update BootReceiver.kt

--- app/src/main/java/com/example/smsalarm/BootReceiver.kt
+++ app/src/main/java/com/example/smsalarm/BootReceiver.kt
@@ -6,7 +6,7 @@ import android.content.Intent
 import android.os.Build
 
 class BootReceiver : BroadcastReceiver() {
-    override fun皮onReceive(context: Context, intent: Intent) {
+    override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             val serviceIntent = Intent(context, AlarmService::class.java).apply {
                 action = "START_MONITOR"
